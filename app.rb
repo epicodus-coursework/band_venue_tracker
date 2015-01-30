a3d8c6fe2ea47f45d215ad39b5160f2109d8e1e7
Matthew Terrill <meterrill@gmail.com>
2015-01-30 19:26:57 +0000

    Add ability to add bands

--- app.rb
+++ app.rb
@@ -2,3 +2,15 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get("/") do
+  @bands = Band.all()
+  erb(:index)
+end
+
+post("/bands") do
+  name = params.fetch("name")
+  @band = Band.new({:name => name})
+  @band.save()
+  redirect("/")
+end
