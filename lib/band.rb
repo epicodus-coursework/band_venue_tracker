9cb3cff28355ad0f7cd08609859c0f0063bcb25d
Matthew Terrill <meterrill@gmail.com>
2015-01-30 22:15:30 +0000

    Add spec to capitalize the first letter of the band name and make it pass

--- lib/band.rb
+++ lib/band.rb
@@ -1,3 +1,10 @@
 class Band < ActiveRecord::Base
   validates(:name, :presence => true)
+  before_save(:capitalize_name)
+
+  private
+
+  define_method(:capitalize_name) do
+    self.name=(name().capitalize!())
+  end
 end
--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -5,4 +5,9 @@ describe(Band) do
     band = Band.new({:name => ""})
     expect(band.save()).to(eq(false))
   end
+
+  it("capitalizes the first letter of the name") do
+    band = Band.create({:name => "new band"})
+    expect(band.name()).to(eq("New band"))
+  end
 end
