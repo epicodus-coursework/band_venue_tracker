db0c6d44c0aadde2a21adb5f888d437ce3586ec8
Matthew Terrill <meterrill@gmail.com>
2015-02-02 19:12:34 +0000

    Add separate pages (bands.erb and venues.erb) to add/update band info

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require("bundler/setup")
 Bundler.require(:default)
 
-Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+Dir[File.dirname(__FILE__) + "/lib/*.rb"].each { |file| require file }
 
 get("/") do
   @bands = Band.all()
@@ -9,12 +9,22 @@ get("/") do
   erb(:index)
 end
 
+get("/bands") do
+  @bands = Band.all()
+  erb(:bands)
+end
+
+get("/venues") do
+  @venues = Venue.all()
+  erb(:venues)
+end
+
 post("/bands") do
   name = params.fetch("name")
   @band = Band.new({:name => name, :done => false})
   @band.save()
   if @band.save()
-    redirect("/")
+    redirect("/bands")
   else
     erb(:band_errors)
   end
@@ -25,16 +35,22 @@ post("/venues") do
   @venue = Venue.new({:name => name, :done => false})
   @venue.save()
   if @venue.save()
-    redirect("/")
+    redirect("/venues")
   else
     erb(:venue_errors)
   end
 end
 
-get('/bands/:id/edit') do
+get("/bands/:id") do
   @band = Band.find(params.fetch("id").to_i())
   @venues = Venue.all()
-  erb(:band_edit)
+  erb(:band_info)
+end
+
+get("/venues/:id") do
+  @venue = Venue.find(params.fetch("id").to_i())
+  @bands = Band.all()
+  erb(:venue_info)
 end
 
 patch("/bands/:id") do
